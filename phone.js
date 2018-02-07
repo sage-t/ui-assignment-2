5b582052b7b162c1c838bee3c451f9c26ca18676
Sage Thomas <sage.thomas@outlook.com>
2018-02-07 11:16:03 -0700

    gesture area setup

--- phone.js
+++ phone.js
@@ -1,6 +1,26 @@
+function show(item, tab) {
+    $(item).show();
+    $(tab).addClass("active-tab");
+}
+
+function hideAll() {
+    $("#content-dialer").hide();
+    $("#content-list").hide();
+    $("#content-form").hide();
+    $("#content-gesture").hide();
+    $(".active-tab").removeClass("active-tab");
+}
+
 function dialerCallback(i) {
-    return function(){
+    return function() {
         $("#phone-number").val($("#phone-number").val() + i);
+    }
+}
+
+function tabCallback(tab_id, content_id) {
+    return function() {
+        hideAll();
+        show(content_id, tab_id);
     }
 }
 
@@ -15,35 +35,18 @@ $(document).ready(function() {
     $('#b-hash').click(dialerCallback('#'));
     $('#b-star').click(dialerCallback('*'));
     $('#b-clear').click(function() {$("#phone-number").val('')});
-    
 
-    $("#dialer-button").click(function() {
-        hideAll();
-        show("#content-dialer", "#dialer-button");
-    });
 
-    $("#list-button").click(function() {
-        hideAll();
-        show("#content-list", "#list-button");
-    });
+    // setup tabs
+    var tab_ids = ["#dialer-button", "#list-button", "#form-button", "#gesture-button"];
+    var content_ids = ["#content-dialer", "#content-list", "#content-form", "#content-gesture"];
+    for (let i = 0; i < tab_ids.length; i++) {
+        $(tab_ids[i]).click(tabCallback(tab_ids[i], content_ids[i]));
+    }
 
-    $("#form-button").click(function() {
-        hideAll();
-        show("#content-form", "#form-button");
-    });
+
+    // setup gesture area
+
+
 });
-
-
-
-function show(item, tab) {
-    $(item).show();
-    $(tab).addClass("active-tab");
-}
-
-function hideAll() {
-    $("#content-dialer").hide();
-    $("#content-list").hide();
-    $("#content-form").hide();
-    $(".active-tab").removeClass("active-tab");
-}
 
